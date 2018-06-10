afab6498480d2a38c989a9e909cbad5e49935137
nahrinoda <32439687+nahrinoda@users.noreply.github.com>
2018-06-10 13:50:16 -0400

    updated folder

---
+++ assets/css/game.js
@@ -0,0 +1,89 @@
+// Game function code //
+
+$(function()
+{
+    var tileSize, i, blank, blankTile, par = $('#cover'), bTileIndex, neighbourTiles = [], nLen, ind;
+
+    function moveImage()
+    {
+        var blankTileIndex, clickedTileIndex, id = $(this).attr('id');
+
+        blankTileIndex = $('.img').index(blankTile);
+        clickedTileIndex = $('.img').index(this);
+
+        if( (blankTileIndex == clickedTileIndex + 1) && (blankTileIndex %4 !== 0 ) )
+        {
+            $('#'+id).before(blankTile);
+        }
+        else if( (blankTileIndex == clickedTileIndex - 1) && (clickedTileIndex %4 !== 0 ) )
+        {
+            $('#'+id).after(blankTile);
+        }
+        else if( (blankTileIndex +4 == clickedTileIndex) || (blankTileIndex == clickedTileIndex+4) )
+        {
+            var prev1 = $('#'+id).prev(), prev2 = blankTile.prev();
+            
+            if( prev1.length === 0 )
+                par.prepend(blankTile);    
+            else
+                $(prev1).after(blankTile);
+            
+            if( prev2.length === 0 )
+                par.prepend($('#'+id));
+            else
+                $(prev2).after($('#'+id));
+        }
+    }
+
+    function startApp()
+    {
+        par.html('');
+
+        for(i=1; i<=16; i++)
+        {
+            par.append('<div class="img" id="img-'+i+'"><img src="https://www.webslake.com/w_img/cp_img/'+i+'.jpg"></div>');
+            $('#img-'+i).on('click',moveImage);
+        }
+
+        blank = Math.floor((Math.random() * 16) + 1);
+        $('#img-'+blank).html('').addClass('blank');
+        
+        blankTile = $('.blank');
+        blankTile.off('click');
+
+        tileSize = $('.img').outerWidth();
+    }
+
+    function shuffle()
+    {
+        for(i=0; i<153; i++)
+        {
+            neighbourTiles = [];
+            bTileIndex = $('.img').index(blankTile);
+        
+            if( (bTileIndex + 1 < 16) && ((bTileIndex + 1) % 4 !== 0 ) )
+                neighbourTiles.push(bTileIndex + 1);
+
+            if( (bTileIndex - 1 > -1) && (bTileIndex % 4 !== 0 ) )
+                neighbourTiles.push(bTileIndex - 1);
+            
+            if( bTileIndex + 4 < 16 )
+                neighbourTiles.push(bTileIndex + 4);
+
+            if( bTileIndex - 4 > -1 )
+                neighbourTiles.push(bTileIndex - 4);
+
+            nLen = neighbourTiles.length;
+
+            ind = Math.floor((Math.random() * nLen));
+    
+            $('#cover .img:eq('+neighbourTiles[ind]+')').trigger('click');
+        }
+    }
+
+    $('#shuffleBtn').on('click',shuffle);
+    $('#restartBtn').on('click',startApp);
+
+    startApp();
+});
+
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Game Time
+Sliding Puzzel Game App.
+
+Team:
+1- Daniel Chen
+2- Dominik Pomichter
+3- Agatha Garces
+4- Nahrin Oda
+
+
+SLIDING PUZZLE BRIEF:-
+
+1-	Landing page will include entering your name and three different difficulty levels to choose from. Difficulty levels will be determined by the number of boxes.
+2-	The pieces are to be pushed around over the board until the picture is complete. 
+3-	Pictures are randomly pulled from Flickr API (https://www.flickr.com/services/api/auth.oauth.html)
+4-	Random music will be playing while you are solving the puzzle, using Fanburst API.
+(https://developers.fanburst.com/#trending_tracks)
+5-	The pieces are numbered so that you will know in which order they should be. The pieces will not be numbered on the harder levels.
+6-	You can only move one piece at a time and you do it by clicking on it.
+7-	The number on each piece tells where in the puzzle it belongs.
+8-	The piece marked 1 should be in the upper left corner of the slide puzzle. 
+9-	Each completed level results in 10 knowledge point. 
+10-	The maximum number of points (30 knowledge points) is achieved when you complete all 3 levels. 
+
